6dff3db26b93f17e7ae7dfb0476bf48b8df8d8e8
hoaeixpz <hoaeixpz@163.com>
2026-07-31 09:54:16 +0800

    set drawdown level

--- QMT/code/all_weather/sim_all_weather_bond_0_1.py
+++ QMT/code/all_weather/sim_all_weather_bond_0_1.py
@@ -47,7 +47,7 @@ weights = {}
 
 # 回撤管理
 record_max = 219202
-max_down_T = [1.2, 1.8, 2.4, 3.0]
+max_down_T = [1.1, 1.7, 2.4, 3.2]
 last_level = 2          # 上次回撤等级（防止同一等级重复触发）
 #force_level = None
 force_level = 2         # 不计算，直接设置level为force_level
